ed6dbb1f9ab93a635961910481b0ee4840c88964
kei077 <kawtartaik123@gmail.com>
2026-02-11 23:05:51 +0100

    feat: add exit, echo and type built-in commands

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,37 @@ fn main() {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut input_buffer : String = String::new();
+        let mut input_buffer = String::new();
         io::stdin().read_line(&mut input_buffer).unwrap();
+        
+        let args: Vec<&str> = input_buffer.split_whitespace().collect();
+        
+        if args.is_empty() {
+            continue;
+        }
 
-        println!("{}: command not found", input_buffer.trim());
+        match args[0] {
+            "exit" => break,
+            "echo" => {
+                if args.len() > 1 {
+                    println!("{}", args[1..].join(" "));
+                } else {
+                    println!();
+                }
+            }
+            "type" => {
+                if args.len() < 2 {
+                    println!("type: missing operand");
+                } else {
+                    match args[1] {
+                        "exit" | "echo" | "type" => {
+                            println!("{} is a shell builtin", args[1]);
+                        }
+                        _ => println!("type: {}: not found", args[1]),
+                    }
+                }
+            }
+            _ => println!("{}: command not found", args[0]),
+        }
     }
 }
