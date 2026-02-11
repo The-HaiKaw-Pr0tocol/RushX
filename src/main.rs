fd8346d740e355856ac0bf0b4d951ff2b933fa94
Kawtar TAIK <116844401+kei077@users.noreply.github.com>
2026-02-11 23:13:01 +0100

    - feat: Initialized the REPL lopp + echo, exit and type built-in commands (#5)
    * feat: implement basic REPL loop with shell prompt
    
    * feat: add exit, echo and type built-in commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+#[allow(unused_imports)]
+use std::io::{self, Write};
+
 fn main() {
-    println!("Ri from RushX !!");
+    println!("Hi from RushX !!");
+
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut input_buffer = String::new();
+        io::stdin().read_line(&mut input_buffer).unwrap();
+        
+        let args: Vec<&str> = input_buffer.split_whitespace().collect();
+        
+        if args.is_empty() {
+            continue;
+        }
+
+        match args[0] {
+            "exit" => break,
+            "echo" => {
+                if args.len() > 1 {
+                    println!("{}", args[1..].join(" "));
+                } else {
+                    println!();
+                }
+            }
+            "type" => {
+                if args.len() < 2 {
+                    println!("type: missing operand");
+                } else {
+                    match args[1] {
+                        "exit" | "echo" | "type" => {
+                            println!("{} is a shell builtin", args[1]);
+                        }
+                        _ => println!("type: {}: not found", args[1]),
+                    }
+                }
+            }
+            _ => println!("{}: command not found", args[0]),
+        }
+    }
 }
